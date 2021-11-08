7a2feb40d50dd9bafca760efbd5b4f3e9f300a09
willian2061 <willianchen2061@gmail.com>
2021-11-08 16:02:02 -0300

    nova tela de login

--- projeto_cc/lib/main.dart
+++ projeto_cc/lib/main.dart
@@ -22,6 +22,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       initialBinding: ApplicationBindings(),
       theme: ThemeData(
--- projeto_cc/lib/modules/login/login_page.dart
+++ projeto_cc/lib/modules/login/login_page.dart
@@ -24,49 +24,121 @@ class LoginPage extends GetView<LoginControler> {
           Container(
             color: Colors.black45,
           ),
-          Padding(
-            padding: const EdgeInsets.only(top: 150.0),
-            child: Column(
-              children: [
-                Image.asset('assets/images/logoOTC.png'),
-                SizedBox(
-                  height: 80,
-                ),
-                SizedBox(
-                  width: Get.width * .7,
-                  height: 50,
-                  child: SignInButton(
-                    Buttons.Google,
-                    shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(100)),
-                    onPressed: () => controller.login(),
-                    text: 'Entrar Com Google',
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(top: 15),
-                  child: SizedBox(
-                    width: Get.width * .7,
-                    height: 50,
-                    child: SignInButton(
-                      Buttons.Email,
-                      shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(100)),
-                      onPressed: () => controller.login(),
-                      text: 'Entrar Com email',
+          SingleChildScrollView(
+            child: Padding(
+              padding: const EdgeInsets.only(top: 150),
+              child: Column(
+                children: [
+                  Image.asset('assets/images/logoOTC.png'),
+
+                  Padding(
+                    padding: const EdgeInsets.only(top: 120),
+                    child: Container(
+                      width: Get.width * .8,
+                      height: 65,
+                      //color: Colors.white,
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.all(Radius.circular(100))),
+                      child: TextFormField(
+                        validator: (value) =>
+                            value!.isEmpty ? "Informe o Usuário" : null,
+                        textAlign: TextAlign.center,
+                        style: const TextStyle(
+                            color: Colors.black, fontSize: 20.0),
+                        decoration: const InputDecoration(
+                          fillColor: Colors.white,
+                          filled: true,
+                          labelText: "Usuário",
+                          labelStyle: TextStyle(color: Colors.black54),
+                        ),
+                      ),
                     ),
                   ),
-                )
-                // Obx(() {
-                //   return Text(
-                //     controller.nome.value,
-                //     style: TextStyle(
-                //       color: Colors.white,
-                //       fontSize: 20,
-                //     ),
-                //   );
-                // })
-              ],
+                  Padding(
+                    padding: const EdgeInsets.only(top: 10),
+                    child: Container(
+                      width: Get.width * .8,
+                      height: 65,
+                      //color: Colors.white,
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.all(Radius.circular(100))),
+                      child: TextFormField(
+                        validator: (value) =>
+                            value!.isEmpty ? "Informe a senha" : null,
+                        textAlign: TextAlign.center,
+                        style: const TextStyle(
+                            color: Colors.black, fontSize: 20.0),
+                        decoration: const InputDecoration(
+                          fillColor: Colors.white,
+                          filled: true,
+                          labelText: "Senha",
+                          labelStyle: TextStyle(color: Colors.black54),
+                        ),
+                      ),
+                    ),
+                  ),
+                  // SizedBox(
+                  //   width: Get.width * .7,
+                  //   height: 50,
+                  //   child: SignInButton(
+                  //     Buttons.Google,
+                  //     shape: RoundedRectangleBorder(
+                  //         borderRadius: BorderRadius.circular(100)),
+                  //     onPressed: () => controller.login(),
+                  //     text: 'Entrar Com Google',
+                  //   ),
+                  // ),
+                  Padding(
+                    padding: const EdgeInsets.only(right: 100, top: 1),
+                    child: TextButton(
+                      child: Text('ainda não tem conta?'),
+                      style: TextButton.styleFrom(
+                        primary: Colors.blue,
+                        fixedSize: Size(Get.width * .5, 5),
+                      ),
+                      onPressed: () {
+                        print('pressed');
+                      },
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(right: 200, top: 10),
+                    child: ElevatedButton(
+                      child: Text('Login'),
+                      style: ElevatedButton.styleFrom(
+                        primary: Colors.grey,
+                        fixedSize: Size(Get.width * .3, 30),
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10)),
+                      ),
+                      onPressed: () {
+                        print('pressed');
+                      },
+                    ),
+
+                    // child: SizedBox(
+                    //   width: Get.width * .7,
+                    //   height: 50,
+                    //   child: SignInButton(
+                    //     Buttons.Email,
+                    //     shape: RoundedRectangleBorder(
+                    //         borderRadius: BorderRadius.circular(100)),
+                    //     onPressed: () => controller.login(),
+                    //     text: 'Entrar Com email',
+                    //   ),
+                  ),
+
+                  // Obx(() {
+                  //   return Text(
+                  //     controller.nome.value,
+                  //     style: TextStyle(
+                  //       color: Colors.white,
+                  //       fontSize: 20,
+                  //     ),
+                  //   );
+                  // })
+                ],
+              ),
             ),
           ),
         ],
